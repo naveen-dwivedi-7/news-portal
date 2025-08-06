41ae0d545037e2ec6da983fad791ce1c60f6b6f8
Naveen Dwivedi <75691459+naveen-dwivedi-7@users.noreply.github.com>
2025-08-06 20:52:08 +0530

    implementing logout functionality

--- app/Http/Controllers/Admin/AdminAuthenticationController.php
+++ app/Http/Controllers/Admin/AdminAuthenticationController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\HandleLoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthenticationController extends Controller
 {
@@ -21,6 +23,15 @@ class AdminAuthenticationController extends Controller
     return redirect()->route('admin.dashboard');
 }
 
+public function logout(Request $request): RedirectResponse
+    {
 
+        Auth::guard('admin')->logout();
 
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
+    }
 }
